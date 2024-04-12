b6488e54d41604f8588c90e1112ca0de104e459a
Dctr-LurS <adrianrg2004@gmail.com>
2024-04-13 00:11:46 +0200

    Implementacion Google Login

--- app/src/main/java/com/example/top_culinary/login/LoginActivity.java
+++ app/src/main/java/com/example/top_culinary/login/LoginActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.icu.number.IntegerWidth;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -17,11 +18,19 @@ import android.widget.Toast;
 import com.example.top_culinary.MainActivity;
 import com.example.top_culinary.R;
 import com.example.top_culinary.registro.RegistroActivity;
+import com.google.android.gms.auth.api.identity.SignInCredential;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class LoginActivity extends AppCompatActivity {
     // Widgets
@@ -41,12 +50,18 @@ public class LoginActivity extends AppCompatActivity {
 
     // Variables
     private FirebaseAuth firebaseAuth;
+    private GoogleSignInClient mGoogleSignInClient;
+
+    // Constantes
+    private static final int RC_SIGN_IN = 9001;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         // Inicializacion del Firebase
         firebaseAuth = FirebaseAuth.getInstance();
+
+        // Implementar el que compruebe si el usuario actual ya ha iniciado sesion a traves del token
 
         // Inicializacion de los widgets
         imageViewFondo = findViewById(R.id.imgFondo);
@@ -62,7 +77,7 @@ public class LoginActivity extends AppCompatActivity {
         textViewIniciarCon = findViewById(R.id.txvIniciarSesionCon);
         imageButtonGoogle = findViewById(R.id.iBtnGoogle);
         imageButtonFacebook = findViewById(R.id.iBtnFacebook);
-        
+
         // Listener del boton para iniciar sesion
         buttonIniciarSesion.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -84,11 +99,26 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
+        // Establecemos las opciones del inicio de sesion de google
+        GoogleSignInOptions googleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this,googleSignInOptions);
+
         // Listener del boton para redirigir al registro
         buttonRegistro.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 registro();
+            }
+        });
+
+        // Listenre del boton para iniciar sesion con google
+        imageButtonGoogle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                inicioSesionGoogle();
             }
         });
     }
@@ -124,5 +154,54 @@ public class LoginActivity extends AppCompatActivity {
     private void registro(){
         Intent intent = new Intent(LoginActivity.this, RegistroActivity.class);
         startActivity(intent);
+    }
+
+    private void inicioSesionGoogle(){
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == RC_SIGN_IN){
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask){
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+            firebaseAuthWithGoogle(account);
+        } catch (ApiException e) {
+            mostrarToast(e.getMessage());
+        }
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount account){
+        AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(),null);
+        firebaseAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            FirebaseUser currentUser = firebaseAuth.getCurrentUser();
+                            mostrarToast("Inicio de Sesion satisfactorio con Google");
+                            startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                            finish();
+                        } else {
+                            mostrarToast("Error en el Inicio de Sesion con Google");
+                        }
+                    }
+                });
+    }
+
+    /**
+     * Mostramos un toast al usuario
+     * @param mensaje del toast
+     */
+    private void mostrarToast(String mensaje){
+        Toast.makeText(LoginActivity.this, mensaje, Toast.LENGTH_SHORT).show();
     }
 }
