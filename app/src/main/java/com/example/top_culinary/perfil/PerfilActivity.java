9d94315addc60c2be52aecb0e5c9efb0339b2afa
Dctr-LurS <adrianrg2004@gmail.com>
2024-05-01 19:56:59 +0200

    Implementacion Layout del Perfil

--- app/src/main/java/com/example/top_culinary/perfil/PerfilActivity.java
+++ app/src/main/java/com/example/top_culinary/perfil/PerfilActivity.java
@@ -5,7 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.example.top_culinary.R;
 import com.example.top_culinary.cesta.CestaActivity;
@@ -15,19 +19,42 @@ import com.example.top_culinary.recetas.AniadirRecetasActivity;
 
 public class PerfilActivity extends AppCompatActivity {
     // Declaracion de los widgets
+    TextView textViewNomUsuario;
+    ImageView imageViewAvatar;
+    TextView textViewSeguidores;
+    TextView textViewNumSeguidores;
+    View viewLineaDivisora;
+    TextView textViewSeguidos;
+    TextView textViewNumSeguidos;
+    TextView textViewNumRecetas;
+    TextView textViewRecetasPublicadas;
+    Button buttonSeguir;
     ImageButton buttonAniadirRecetas;
     ImageButton buttonCesta;
     ImageButton buttonCocina;
     ImageButton buttonForo;
+    LinearLayout linearLayoutBotoneraInferior;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfil);
         // Botones de la botonera inferior
+        textViewNomUsuario = findViewById(R.id.textViewNomUsuario);
+        imageViewAvatar = findViewById(R.id.imageViewAvatar);
+        imageViewAvatar.setImageResource(R.drawable.avatar);
+        textViewSeguidores = findViewById(R.id.textViewSeguidores);
+        textViewNumSeguidores = findViewById(R.id.textViewNumSeguidores);
+        viewLineaDivisora = findViewById(R.id.viewLineaDivisora);
+        textViewSeguidos = findViewById(R.id.textViewSeguidos);
+        textViewNumSeguidos = findViewById(R.id.textViewNumSeguidos);
+        textViewNumRecetas = findViewById(R.id.textViewNumRecetas);
+        textViewRecetasPublicadas = findViewById(R.id.textViewRecetas);
+        buttonSeguir = findViewById(R.id.buttonSeguir);
         buttonAniadirRecetas = findViewById(R.id.imgBRecetas);
         buttonCesta = findViewById(R.id.imgBCesta);
         buttonCocina = findViewById(R.id.imgBCocina);
         buttonForo = findViewById(R.id.imgBForo);
+        linearLayoutBotoneraInferior = findViewById(R.id.linearLayoutBotoneraInferior);
         // Listener de los botones inferiores
         buttonAniadirRecetas.setOnClickListener(new View.OnClickListener() {
             @Override
